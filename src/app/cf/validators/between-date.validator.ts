fa0f2df6bbd430ed1f7c3928e965938d6e0e7d77
Valera <gavrilow777@gmail.com>
2020-08-03 16:44:48 +0300

    fix interval

--- src/app/cf/validators/between-date.validator.ts
+++ src/app/cf/validators/between-date.validator.ts
@@ -1,13 +1,9 @@
 import { ValidatorFn, AbstractControl } from '@angular/forms';
 import { isAfter, isBefore } from '../utils/date-time';
-export function betweenDateValidator(date: Date): ValidatorFn {
+export function betweenDateValidator(dateIntreval: [Date, Date]): ValidatorFn {
   return (control: AbstractControl): { [key: string]: any } | null => {
-    console.log('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>');
-    console.log('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>');
     const controlDate = control.value;
-    console.log(controlDate);
-    console.log(date);
-    if (!isAfter(controlDate, date) && !isBefore(controlDate, date)) {
+    if (!isAfter(controlDate[0], dateIntreval[0]) && !isBefore(controlDate[1], dateIntreval[1])) {
       return null;
     }
     return { betweenDate: true };
